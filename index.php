1b9d5d20f5092af4f34de16f520dbb7ac855e856
Anubha Dubey <anubhadubeyy@gmail.com>
2022-03-01 00:20:44 -0500

    fixed error in bestseller product boundary, added css for the same

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
 // DEVELOPER                      DATE                        COMMENTS
 // Anubha Dubey(2032178)          2022-02-26                Added php common functions, company name and description
-// Anubha Dubey(2032178)          2022-02-28                Updated best seller product image to appear 100% bigger
-
+// Anubha Dubey(2032178)          2022-02-28                Updated best seller product image to appear 100%
+// Anubha Dubey(2032178)          2022-02-29                Fixed error in best seller product image, added CSS for the same 
 //
 
 
@@ -28,7 +28,7 @@ TopPage("Home Page");
 $product_images  = PRODUCT_IMAGES;
 shuffle($product_images);
 //print_r($product_images);
-?>
+?> <!-- php tag ends here -->
 
 <div class="company-container">
     <h1 class="company-name">🍒🫐🍓 YOGO Yogurt 🍒🫐🍓</h1>
@@ -37,17 +37,45 @@ shuffle($product_images);
      Doloremque reprehenderit, <br> laboriosam totam blanditiis temporibus adipisci 
       quos voluptates corporis, cum iure, <br> obcaecati culpa et sint magni sapiente eveniet natus cupiditate perspiciatis?</p>
 </div>
-<a href="https://www.google.ca/" target="_blank"> 
+
+
+<!-- <style>
+    .bestseller-img{        
+    border: 5px solid red;
+    width: 300px;
+    padding: 2%;
+    margin-top: 2%;
+    margin-bottom: 4%;
+    background-color: #EFDAD7;
+</style> -->
+
+
+<?php
+if($product_images[0]=="strawberry-yogo.png"){
+    echo '<img alt="yogurt-img" class="bestseller-img" src="Images/'.$product_images[0].'"/>';
+}
+else{
+    echo '<img alt="yogurt-img" class="product-img" src="Images/'.$product_images[0].'"/>';
+}    
+?>
+
+<!-- DO NOT REMOVE-->
+<?php
+    //echo '<img alt="yogurt-img" class="product-img" src="Images/'.$product_images[0].'"/>';
+?>
+
+<!-- // DO NOT REMOVE -->
+<!-- <a href="https://www.google.ca/" target="_blank"> 
     <img src="<?php 
-    if($product_images[0]==$bestSellerProduct){
-        echo FOLDER_IMAGES . $bestSellerProduct;
-   }
-    // echo FOLDER_IMAGES . $product_images[0];
-    else{
-        echo FOLDER_IMAGES . $product_images[0];
-    }
+    // if($product_images[0]==$bestSellerProduct){
+    //     echo FOLDER_IMAGES . $bestSellerProduct;
+    // }
+    // else{
+    //     echo FOLDER_IMAGES . $product_images[0];
+    // }
     
-    ?>" class="product-img" alt="yogurt image"> </a>
+    ?>" class="product-img" alt="yogurt image"> </a> -->
+
 <?php
 footer();
 ?>
